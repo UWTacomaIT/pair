280aece4739e38062f725a244705af4fd6d76c31
daniilf <daniilf@uw.edu>
2025-03-18 12:12:07 -0700

    Updating

--- src/view/Index/Publications.jsx
+++ src/view/Index/Publications.jsx
@@ -6,7 +6,6 @@ import Footer from "../../components/footer/Index";
 
 import data from "../../../public/data.json"
 
-console.log(data.main.FacultyList)
 const index = () => {
     const navigate = useNavigate()
 
@@ -25,17 +24,51 @@ const index = () => {
                     Publications
                 </div>
             </div>
-            <div className={'mx-auto w-11/12 md:w-10/12 my-20'}>
-
-                <div className='w-11/12 mx-auto md:w-6/12'>
-                    {
-                        data.main.publications.map((item, index) => <div key={item.content + index} dangerouslySetInnerHTML={{ __html: item.content }} 
-                            className='first:mt-0 mt-12 text-center shadow-md rounded-md hover:shadow-lg transition-all p-8'
-                        >
-                        </div>)
-                    }
-                </div>
-
+            <div className={'mx-auto w-11/12 md:w-7/12 my-20'}>
+                {
+                    data.main.PublicationList && data.main.PublicationList.map((item, index) => (
+                        <div className='first:mt-0 mt-10' key={item.title + index}>
+                            {
+                                item.url && item.url !== "None" ?
+                                    <p className='text-2xl flex items-center' 
+                                       onClick={() => window.open(item.url)}
+                                       style={{ color: '#4aa1d4', cursor: 'pointer' }}>
+                                        <span className="hover:underline">{item.title}</span>
+                                        <span className="animated_circle ml-2" aria-hidden="true">
+                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                 xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                                 width="24px" height="24px" viewBox="0 0 24 24" xmlSpace="preserve">
+                                                <g className="animated_icon_arrow" fill="none" stroke="#2175FF"
+                                                   strokeWidth="1.5">
+                                                    <path transform="translate(6, 12)"
+                                                          d="M9.0075 2.25H0V3.75H9.0075V6L12 3L9.0075 0V2.25Z"
+                                                          fill="#002D72"></path>
+                                                </g>
+                                                <circle className="animated_icon_circle" transform="rotate(-90)"
+                                                        cx="11" cy="11" r="11" fill="transparent" stroke="#2175FF"
+                                                        strokeWidth="1"></circle>
+                                            </svg>
+                                        </span>
+                                    </p> :
+                                    <p className='text-2xl flex items-center' style={{ color: '#4aa1d4' }}>
+                                        {item.title}
+                                    </p>
+                            }
+                            <p className='my-3 text-sm text-gray-700'>
+                                {item.year} • {item.venue}
+                            </p>
+                            {item.authors && (
+                                <p className='mb-3'>
+                                    <span className="font-medium">Authors:</span> {item.authors}
+                                </p>
+                            )}
+                            {
+                                index !== (data.main.PublicationList.length - 1) &&
+                                <div className={'bg-black bg-opacity-15 mt-10'} style={{ height: '1px' }}></div>
+                            }
+                        </div>
+                    ))
+                }
             </div>
         </main>
         <Footer />
